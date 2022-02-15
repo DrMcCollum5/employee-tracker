603f08f5fb39cfcd0ff64060cf33cac245f57628
DrMcCollum5 <94315015+DrMcCollum5@users.noreply.github.com>
2022-02-14 23:32:30 -0500

    updated seeds file

--- db/schema.sql
+++ db/schema.sql
@@ -2,9 +2,9 @@ DROP DATABASE IF EXISTS employees_db;
 
 CREATE DATABASE employees_db;
 
-USE employees_db;
+USE employees;
 
-CREATE TABLE department(
+CREATE TABLE department (
     id INT NOT NULL AUTO_INCREMENT, PRIMARY KEY(id),
     name VARCHAR(30),
     
@@ -20,12 +20,15 @@ CREATE TABLE role (
     FOREIGN KEY(department_id) REFERENCES department(id) ON DELETE CASCADE
 );
 
-CREATE TABLE employee(
+CREATE TABLE employee (
     id INT NOT NULL AUTO_INCREMENT, PRIMARY KEY (id),
     first_name VARCHAR(30) NOT NULL,
     last_name VARCHAR(30) NOT NULL,
-    role_id INT NULL,
+    role_id INT NULL, UNSINED NOT NULL,
+    INDEX role_id,
+    CONSTRAINT fk_role 
     manager_id INT NULL,
+    INDEX man_ind (manager_id)
     FOREIGN KEY (roles_id) REFERENCES roles (id),
     FOREIGN KEY (manager_id) REFERENCES employee (id)
 );
---
+++ db/seeds.sql
@@ -0,0 +1,34 @@
+use employees;
+
+INSERT INTO department;
+(name)
+VALUES
+  ('Sales')
+  ('Engineering')
+  ('Finance')
+  ('Legal')
+
+INSERT INTO role
+  (title, salary, department_id)
+VALUES
+  ('Sales Lead', 100000, 1),
+  ('Salesperson', 80000, 1),
+  ('Lead Engineer', 150000, 2),
+  ('Software Engineer', 120000, 2),
+  ('Lawyer' 190000, 3),
+  ('Legal Team Lead', 250000, 3),
+  ('Account Manager', 1600000, 4),
+  ('Accountant', 125000, 4);
+
+  INSERT INTO employee
+  (first_name, last_name, role_id, manager_id)
+
+  VALUES 
+    ('Gia', 'Malone', 1, Null),
+    ('Blippi', 'Doe', 2, 1),
+    ('Winter', 'Storm', 3, Null),
+    ('Butter', 'Scotch', 4, 3),
+    ('Sarah', 'White', 5, Null),
+    ('Snow', 'Flake', 6, 5),
+    ('Muscle', 'Mann' 7, Null),
+    ('Ethan', 'Allen', 8, 7);
