fb945f2c8a9eef100ff5e701d69b38238418bbf0
DrMcCollum5 <94315015+DrMcCollum5@users.noreply.github.com>
2022-02-14 23:11:51 -0500

    updated schema file

--- db/schema.sql
+++ db/schema.sql
@@ -5,25 +5,26 @@ CREATE DATABASE employees_db;
 USE employees_db;
 
 CREATE TABLE department(
-    id INT NOT NULL AUTO_INCREMENT,
+    id INT NOT NULL AUTO_INCREMENT, PRIMARY KEY(id),
     name VARCHAR(30),
-    PRIMARY KEY(id)
+    
 );
 
-CREATE TABLE roles(
-    id INT NOT NULL AUTO_INCREMENT,
+CREATE TABLE role (
+    id INT NOT NULL AUTO_INCREMENT, PRIMARY KEY(id),
     title VARCHAR(30),
     salary DECIMAL,
     department_id INT NULL,
-    PRIMARY KEY(id),
-    FOREIGN KEY(department_id) REFERENCES department(id)
+    INDEX department_id,
+    CONSTRAINT fk_department
+    FOREIGN KEY(department_id) REFERENCES department(id) ON DELETE CASCADE
 );
 
 CREATE TABLE employee(
     id INT NOT NULL AUTO_INCREMENT, PRIMARY KEY (id),
-    first_name VARCHAR(30) NULL,
-    last_name VARCHAR(30) NULL,
-    roles_id INT NULL,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
+    role_id INT NULL,
     manager_id INT NULL,
     FOREIGN KEY (roles_id) REFERENCES roles (id),
     FOREIGN KEY (manager_id) REFERENCES employee (id)
